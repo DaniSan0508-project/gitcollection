74bb7671d497a44c7978dffdb89511e46801b1fb
Daniel <danielsantoscarneiro0508@gmail.com>
2021-09-17 13:41:34 -0300

    adicionado estilo a pagina do Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,15 @@
-import { Title } from "./styled";
+import { Title, Form } from "./styled";
+import logo from "../../assets/logo.svg";
 
 export const Dashboard: React.FC = () => {
-  return <Title>Dashboard</Title>;
+  return (
+    <>
+      <img src={logo} alt="logo-gitcollection" />
+      <Title>Catálogo de repositório do Github</Title>
+      <Form>
+        <input type="text" placeholder="username/repository_name" />
+        <button type="submit">Buscar</button>
+      </Form>
+    </>
+  );
 };
